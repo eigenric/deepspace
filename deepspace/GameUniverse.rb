a9eb0f3a25b7ba62c376cc8a388738825fe4b66c
Ricardo Ruiz <riki319@gmail.com>
2019-03-24 14:31:46 +0100

    Implementado GameUniverse

---
+++ deepspace/GameUniverse.rb
@@ -0,0 +1,63 @@
+require_relative 'GameUniverseToUI'
+require_relative 'GameStateController'
+require_relative 'Dice'
+
+module Deepspace
+
+class GameUniverse
+    attr_reader :gameState
+    @@WIN = 10
+    
+    def initialize()
+        @gameState = GameStateController.new
+        @turns = 0
+        @dice = Dice.new
+        @currentStationIndex = 0 #?
+        @spaceStations = []
+        @currentStation = nil
+    end
+
+    def init_or_aftercombat()
+        @gameState.state == GameState::INIT || @gameState.state == GameState::AFTERCOMBAT
+    end
+
+    def discardHangar()
+        @currentStation.discardHangar() unless init_or_aftercombat
+    end
+
+    def discardShieldBooster(i)
+        @currentStation.discardShieldBooster(i) unless init_or_aftercombat
+    end
+
+    def discardShieldBoosterInHangar(i)
+        @currentStation.discardShieldBoosterInHangar(i) unless init_or_aftercombat
+    end
+
+    def discardWeapon(i)
+        @currentStation.discardWeapon(i) unless init_or_aftercombat
+    end
+
+    def discardWeaponInHangar(i)
+        @currentStation.discardWeaponInHangar(i) unless init_or_aftercombat
+    end
+
+    def mountShieldBooster(i)
+        @currentStation.mountShieldBooster(i) unless init_or_aftercombat
+    end
+
+    def mountWeapon(i)
+        @currentStation.mountWeapon(i) unless init_or_aftercombat
+    end
+
+    def haveAWinner()
+        @currentStation.nMedals == 10
+    end
+
+    def getUIVersion()
+        GameUniverseToUI.new self
+    end
+
+    private :init_or_aftercombat
+end # class
+
+end # module
