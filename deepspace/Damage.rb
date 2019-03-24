d3c74ee3211f853cac2ac215ff20954685563e07
Ricardo Ruiz <riki319@gmail.com>
2019-03-24 14:30:01 +0100

    Modificada clase Damage

--- deepspace/Damage.rb
+++ deepspace/Damage.rb
@@ -4,13 +4,18 @@ class Damage
 
     attr_reader :nShields, :nWeapons, :weapons, :damage
 
+    def initialize(nWeapons, nShields, weapons)
+        @nWeapons = nWeapons
+        @nShields = nShields
+        @weapons = weapons
+    end
+
     def self.newNumericWeapons(w, s)
-        @nWeapons = n
-        @nShields = s
+        new(w, s, nil)
     end
 
     def self.newSpecificWeapons(wl, s)
-
+        new(-1, s, wl)
     end
 
     def self.newCopy(d)
@@ -18,24 +23,51 @@ class Damage
     end
 
     def discardWeapon(w)
-        
+        if @weapons != nil # Lista de weapon
+            @weapons.delete w.type
+        else
+            @nWeapons -= if @nWeapons >= 1 then 1 else 0
+        end
     end
 
     def discardShieldBooster
+       @nWeapons -= if @nWeapons >= 1 then 1 else 0
     end
 
     def hasNoEffect?
+        specific_zero = (@nWeapons == 0 && @weapons == nil)
+        numeric_zero = (@nWeapons == -1 && @weapons.empty?)
+        @nShields && (specific_zero || numeric_zero)
     end
 
     def arrayContainsType(weapons, wt)
+        if weapons.find {|w| w.type == wt }.nil?
+            weapons.index wp
+        else
+            -1
+        end
     end
 
     def adjust(weapons, s)
+        adjustedWeapons = @weapons
+        if @nWeapons == -1 # Hay lista especifica
+            adjustedWeapons = weapons.map do |w|
+                w.type if @weapons.include? w.type 
+            end
+            adjustedWeapons.compact!
+        end
+        self.new(adjustedWeapons, s.length)
     end
 
     def getUIVersion()
-        return DamageToUI.new self
+        DamageToUI.new self
     end
+
+    def to_s()
+        Damage
+    end
+
+    private_class_method :new
 
 
 end # Class
